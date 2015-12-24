cd6bd6adc279946ea817ffcc51a7819b004705ff
Juho Jaakkola <juho@elgg.org>
2015-12-24 15:29:34 +0200

    All communications logic has now been moved to a separate plugin

--- start.php
+++ start.php
@@ -16,53 +16,6 @@ function live_notifier_init() {
 	elgg_require_js('live_notifier/live_notifier');
 
 	elgg_register_plugin_hook_handler('send', 'notification:notifier', 'live_notifier_notification_send');
-
-	elgg_register_plugin_hook_handler('view', 'js/initialize_elgg', 'live_notifier_add_token');
-}
-
-/**
- * Extend javascript config with token usable for subscribing
- *
- * @param string $hook    'view'
- * @param string $type    'js/initialize_elgg'
- * @param string $content Current content
- * @param array  $params  Array containing view, vars and viewtype
- * @return string $content Modified content
- */
-function live_notifier_add_token($hook, $type, $content, $params) {
-	if (!elgg_is_logged_in()) {
-		return $content;
-	}
-
-	$user = elgg_get_logged_in_user_entity();
-	$site_guid = elgg_get_site_entity()->guid;
-
-	//$token = live_notifier_get_token($user_guid);
-	$tokens = live_notifier_get_token_service();
-	$token = $tokens->getToken($user);
-
-	if (!$token) {
-		$token = $tokens->createToken($user);
-	}
-
-	// TODO Check if user has enabled notifier. There's no need for
-	// a token if user doesn't want to receive notifications.
-
-	$content .= "elgg.config.site_guid = {$site_guid};";
-	$content .= "elgg.live_notifier_token = '{$token}';";
-
-	return $content;
-}
-
-/**
- *
- */
-function live_notifier_get_token_service() {
-	global $CONFIG;
-
-	$db = _elgg_services()->db;
-
-	return new \LiveNotifier\Tokens($db, $CONFIG);
 }
 
 /**
@@ -114,14 +67,12 @@ function live_notifier_notification_send($hook, $type, $object, $params) {
 	}
 	elgg_set_ignore_access($ia);
 
-
-	require __DIR__ . '/vendor/autoload.php';
-
 	$context = new ZMQContext();
 	$socket = $context->getSocket(ZMQ::SOCKET_PUSH, 'my pusher');
 	$socket->connect("tcp://localhost:5555");
 
 	$msg = new \stdClass();
+	$msg->consumer = 'live_notifier';
 	$msg->recipient_guid = $recipient->guid;
 	$msg->subject_name = $actor->getDisplayName();
 	$msg->subject_url = $actor->getURL();
