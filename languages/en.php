d52311216f109facb5f7eada9ec6dee4d2853671
Juho Jaakkola <juho.jaakkola@mediamaisteri.com>
2016-02-04 13:44:15 +0200

    Notification about new likes now has a custom translation

---
+++ languages/en.php
@@ -0,0 +1,5 @@
+<?php
+
+return array(
+	'river:create:annotation:likes' => '%s likes your post %s',
+);
---
+++ languages/fi.php
@@ -0,0 +1,5 @@
+<?php
+
+return array(
+	'river:create:annotation:likes' => '%s tykkää kohteestasi %s',
+);
